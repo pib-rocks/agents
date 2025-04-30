9b670ac9621f3f7772667b7914e915fe30d1d697
jueba <juerphen@gmail.com>
2025-04-30 13:42:56 +0200

    Creating and updating of issues finished

--- Product-Owner/agent.py
+++ Product-Owner/agent.py
@@ -2,11 +2,14 @@ import datetime
 from zoneinfo import ZoneInfo
 from google.adk.agents import Agent
 # Import the Jira tools from the tools directory
-from ..tools.jira_tools import get_jira_issue_details, update_jira_issue
+
+import sys
+sys.path.append("..")
+from tools.jira_tools import get_jira_issue_details, update_jira_issue
 
 root_agent = Agent(
     name="jira_agent", # Renamed for clarity
-    model="gemini-2.0-flash",
+    model="gemini-2.0-flash-live-001",
     description=(
         "Agent to manage Jira issues (retrieving and updating)."
     ),
